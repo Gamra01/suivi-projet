b1e4215da5a96889591dacdd216261185550958f
Gamra <ilyassstudies2017@gmail.com>
2020-02-08 13:57:54 +0100

    files

---
+++ menu.py
@@ -0,0 +1,8 @@
+import pygame
+import time
+import random
+from settings import *
+
+pygame.init()
+
+gameDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
---
+++ settings.py
@@ -0,0 +1,27 @@
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+#pygame settings
+
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+
+TITLE = "b1 info"
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+
+# joueur settings
+
+PLAYER_SPEED = 300
